859acbdd45e73174e60109c40ce40bb10cc54d99
leiserfg <leiserfg@gmail.com>
2015-04-21 11:32:03 -0400

    project template

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from lic import __version__
+
+
+
+setup(
+    name='lic',
+    version=__version__,
+    author='Leiser Fernández Gallo',
+    author_email='leiserfg@gmail.com',
+    include_package_data = True,
+    packages=find_packages(),
+    description='A tool to help choosing licenses',
+    url='github.com',   
+    #long_description=open('README.rst').read(),
+    #license='BSD License',
+    #entry_points = {
+    #    'console_scripts': ['lic = lic.cli:main'],
+    #},
+    install_requires=[
+        'toml',
+        'click'
+        ]
+)
